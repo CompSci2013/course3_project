67793c97dc64c6855b30457eb0b981ceb729ccfc
CompSci2013 <kcollins2004@gmail.com>
2021-02-20 23:10:59 +0000

    add Logic and test

---
+++ src/main/java/com/course3/caltech/kc_demo/KcDemoApplication.java
@@ -0,0 +1,13 @@
+package com.course3.caltech.kc_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KcDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KcDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/course3/caltech/kc_demo/KcDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course3.caltech.kc_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KcDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
